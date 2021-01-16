6cd513b1203023f5afbb117225c2d156489d252a
shivam-98 <45638930+shivam-98@users.noreply.github.com>
2021-01-16 22:00:37 +0530

    Add files via upload

---
+++ WA_Spam/WA_spawm.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+
+driver=webdriver.Chrome("Downloads/chromedriver") #Enter Location of Chrome driver
+driver.maximize_window()
+driver.get("https://web.whatsapp.com/")
+sleep(15)
+driver.find_element_by_xpath("/html/body/div/div/div/div/div/div/div[1]/div/div/div[1]/div/div/div").click()
+sleep(5)
+fileLocation= "Downloads/Joker_script.docx"  #Enter location of movie script file
+with open(fileLocation,"r") as file:
+    for line in file:
+        for word in line.split(" "):
+            driver.find_element_by_xpath("//*/footer/div[1]/div[2]/div/div[2]").send_keys(word)
+            driver.find_element_by_xpath("//*/footer/div[1]/div[2]/div/div[2]").send_keys(Keys.ENTER)
